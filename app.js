1b8be3ca01d23605f98a434c34e84127ac9881e8
Justin Thareja <justin.thareja@gmail.com>
2021-02-25 15:13:22 -0500

    passing in component props

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const Pet = () => {
+const Pet = (props) => {
     return React.createElement("div", {}, [
-        React.createElement("h1", {}, "Frida"),
-        React.createElement("h2", {}, "Cat"),
-        React.createElement("h2", {}, "Tabby"),
+        React.createElement("h1", {}, props.name),
+        React.createElement("h2", {}, props.animal),
+        React.createElement("h2", {}, props.breed),
     ]);
 }
 
 const App = () => {
     return React.createElement(
         "div",
-        { id: "something-cool" }, 
+        { id: "something-cool" },
         [
             React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet),
-            React.createElement(Pet),
-            React.createElement(Pet),
+            React.createElement(Pet, { name: "Frida", animal: "Cat", breed: "Tabby" }),
+            React.createElement(Pet, { name: "Diego", animal: "Cat", breed: "Mixed" }),
+            React.createElement(Pet, { name: "Patches", animal: "Cat", breed: "Orange" }),
         ]
     );
 };
