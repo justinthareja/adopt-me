718f3fc1bfd819214c01687edfc774528ea0a7d0
Justin Thareja <justin.thareja@gmail.com>
2021-02-25 16:45:39 -0500

    demonstrates jsx composite components

--- src/app.js
+++ src/app.js
@@ -2,17 +2,15 @@ import React from "react";
 import { render } from "react-dom";
 import Pet from "./Pet";
 
-const App = () => {
-  return React.createElement("div", { id: "something-cool" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, { name: "Frida", animal: "Cat", breed: "Tabby" }),
-    React.createElement(Pet, { name: "Diego", animal: "Cat", breed: "Mixed" }),
-    React.createElement(Pet, {
-      name: "Patches",
-      animal: "Cat",
-      breed: "Orange",
-    }),
-  ]);
-};
+function App() {
+  return (
+    <div>
+      <h1 id="something-cool">Adopt Me!</h1>
+      <Pet name="Frida" animal="Cat" breed="Tabby" />
+      <Pet name="Diego" animal="Cat" breed="Mixed" />
+      <Pet name="Patches" animal="Cat" breed="Orange" />
+    </div>
+  );
+}
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
