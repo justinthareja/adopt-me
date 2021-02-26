d6b2c22c0f91c6d6882c589e5a8521f373199f54
Justin Thareja <justin.thareja@gmail.com>
2021-02-25 19:19:13 -0500

    adds useEffect interaction with pets API

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,12 +1,32 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
 
 function SearchParams() {
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+  // initial render happens first then runs this effect
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+
+    fetchBreeds();
+
+    async function fetchBreeds() {
+      try {
+        const { breeds } = await pet.breeds(animal);
+        const breedStrings = breeds.map(({ name }) => name);
+        setBreeds(breedStrings);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    // takes a list of dependencies that when updated this useEffect() gets re-run
+  }, [animal, setBreed, setBreeds]);
 
   return (
     <div className="search-params">
