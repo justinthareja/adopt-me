c9461bc6870a27eb306360643ad51a09a2c486c2
Justin Thareja <justin.thareja@gmail.com>
2021-02-27 13:25:14 -0500

    added todo list

--- src/app.js
+++ src/app.js
@@ -1,12 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
-import SearchParams from "./SearchParams";
+
+function List({ items = [], ...props }) {
+  return (
+    <ul className="list" {...props}>
+      {items.length > 0 &&
+        items.map(({ value, completed }, i) => {
+          const classNames = `list-item ${
+            completed && " list-item--completed"
+          }`;
+
+          return (
+            <li
+              id={i + 1}
+              key={value.replace(" ", "") + i}
+              className={classNames}
+            >
+              {value}
+            </li>
+          );
+        })}
+    </ul>
+  );
+}
+
+function removeTodos(todos = [], todoId) {
+  const index = todos.findIndex(({ id }) => id === todoId);
+  return [...todos.slice(0, todoId), ...todos.slice(todoId + 1)];
+}
+
+function updateTodos(todos = [], newTodo = {}) {
+  const todoIndex = todos.findIndex((t) => t.id === newTodo.id);
+  const todo = todos[todoIndex];
+  const todoToBeUpdated = { ...todo, ...newTodo };
+
+  if (!todo) return;
+
+  return [
+    ...todos.slice(0, todoIndex),
+    todoToBeUpdated,
+    ...todos.slice(todoIndex + 1),
+  ];
+}
+
+function addTodo(todos = [], value = "") {
+  const newTodo = {
+    id: todos.length + 1,
+    completed: false,
+    value,
+  };
+
+  return [...todos, newTodo];
+}
 
 function App() {
+  const [text, setText] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handleInputKeyUp(e) {
+    if (e.key !== "Enter") return;
+    setTodos(addTodo(todos, e.target.value));
+    setText("");
+  }
+
+  function handleListClick(e) {
+    const todo = todos.find((t) => t.id == e.target.getAttribute("id"));
+    setTodos(updateTodos(todos, { ...todo, completed: !todo.completed }));
+  }
+
   return (
-    <div>
-      <h1 id="something-cool">Adopt Me!</h1>
-      <SearchParams />
+    <div className="app">
+      <label htmlFor="todo">
+        <span>Add A New Todo:</span>
+        <input
+          type="text"
+          name="todo"
+          id="todo"
+          value={text}
+          placeholder="Pick up groceries..."
+          onChange={(e) => setText(e.target.value)}
+          onBlur={(e) => setText(e.target.value)}
+          onKeyUp={handleInputKeyUp}
+        />
+      </label>
+      <List items={todos} onClick={handleListClick} />
     </div>
   );
 }
