5c42a777fbc4d7b8e4cd613e950521aca7d79b17
Justin Thareja <justin.thareja@gmail.com>
2021-03-01 17:52:17 -0500

    added ability to delete todos

--- src/app.js
+++ src/app.js
@@ -5,42 +5,20 @@ function List({ items = [], ...props }) {
   return (
     <ul className="list" {...props}>
       {items.length > 0 &&
-        items.map(({ value, completed }, i) => {
-          const classNames = `list-item ${
+        items.map(({ value, completed, id }, i) => {
+          const classNames = `list-item js-list-item ${
             completed && " list-item--completed"
           }`;
 
           return (
-            <li
-              id={i + 1}
-              key={value.replace(" ", "") + i}
-              className={classNames}
-            >
+            <li id={id} key={value.replace(" ", "") + i} className={classNames}>
               {value}
+              <button className="list-remove js-list-remove">X</button>
             </li>
           );
         })}
     </ul>
   );
-}
-
-function removeTodos(todos = [], todoId) {
-  const index = todos.findIndex(({ id }) => id === todoId);
-  return [...todos.slice(0, todoId), ...todos.slice(todoId + 1)];
-}
-
-function updateTodos(todos = [], newTodo = {}) {
-  const todoIndex = todos.findIndex((t) => t.id === newTodo.id);
-  const todo = todos[todoIndex];
-  const todoToBeUpdated = { ...todo, ...newTodo };
-
-  if (!todo) return;
-
-  return [
-    ...todos.slice(0, todoIndex),
-    todoToBeUpdated,
-    ...todos.slice(todoIndex + 1),
-  ];
 }
 
 function addTodo(todos = [], value = "") {
@@ -53,19 +31,49 @@ function addTodo(todos = [], value = "") {
   return [...todos, newTodo];
 }
 
+function getTodo(todos, id) {
+  return todos.find((todo) => todo.id === id);
+}
+
+function updateTodos(todos = [], newTodo = {}) {
+  const todoIndex = todos.findIndex((t) => t.id === newTodo.id);
+  if (!todos[todoIndex]) return;
+
+  const todo = { ...todos[todoIndex], ...newTodo };
+
+  return [...todos.slice(0, todoIndex), todo, ...todos.slice(todoIndex + 1)];
+}
+
+function removeTodo(todos = [], todoId) {
+  const i = todos.findIndex(({ id }) => id === todoId);
+
+  return [...todos.slice(0, i), ...todos.slice(i + 1)];
+}
+
 function App() {
   const [text, setText] = useState("");
   const [todos, setTodos] = useState([]);
 
   function handleInputKeyUp(e) {
-    if (e.key !== "Enter") return;
+    if (e.key !== "Enter" || e.target.value === "") return;
+
     setTodos(addTodo(todos, e.target.value));
     setText("");
   }
 
   function handleListClick(e) {
-    const todo = todos.find((t) => t.id == e.target.getAttribute("id"));
-    setTodos(updateTodos(todos, { ...todo, completed: !todo.completed }));
+    if (e.target.matches(".js-list-remove")) {
+      const todoId = Number(e.target.parentElement.getAttribute("id"));
+      const result = removeTodo(todos, todoId);
+
+      setTodos(result);
+    }
+
+    if (e.target.matches(".js-list-item")) {
+      const todo = getTodo(todos, Number(e.target.getAttribute("id")));
+
+      setTodos(updateTodos(todos, { ...todo, completed: !todo.completed }));
+    }
   }
 
   return (
