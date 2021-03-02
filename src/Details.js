ee895a60993deeec9cfdfd5186b2ab06baf26c14
Justin Thareja <justin.thareja@gmail.com>
2021-03-02 15:44:07 -0500

    updated details page

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,46 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
 
-function Details(props) {
-  return (
-    <pre>
-      <code>{JSON.stringify(props, null, 4)}</code>
-    </pre>
-  );
+class Details extends React.Component {
+  // public class property
+  state = { loading: true };
+
+  async componentDidMount() {
+    try {
+      const { animal } = await pet.animal(this.props.id);
+
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        loading: false,
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h1>loading ...</h1>;
+    }
+
+    const { animal, breed, location, name, description } = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Details;
