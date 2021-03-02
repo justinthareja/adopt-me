fcf2d3d87adc5eace7cb9d6ccc14a5769e9b4574
Justin Thareja <justin.thareja@gmail.com>
2021-03-02 16:03:49 -0500

    adds Carousel

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,6 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
+import Carousel from "./Carousel";
 
 class Details extends React.Component {
   // public class property
@@ -28,10 +29,11 @@ class Details extends React.Component {
       return <h1>loading ...</h1>;
     }
 
-    const { animal, breed, location, name, description } = this.state;
+    const { animal, breed, location, name, description, media } = this.state;
 
     return (
       <div className="details">
+        <Carousel media={media} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
